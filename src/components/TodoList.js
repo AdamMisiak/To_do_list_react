21a09bbf5e3cfa7d04631d841cb2f113d4afb1b3
AdamMisiak <adammisiak3@gmail.com>
2020-10-09 09:34:15 +0200

    TodoList component added

---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TodoList = () => {
+    return (
+    <div className="task-container">
+        <ul id="task-list" className="task-list">
+            {/* ADDING DIVS WITH LI AND BUTTONS HERE */}
+        </ul>
+    </div>
+    );
+};
+
+
+export default TodoList;
