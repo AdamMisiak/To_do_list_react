fc266c3fe95d05c175bed74d6e357040574289e2
AdamMisiak <adammisiak3@gmail.com>
2020-12-16 15:34:36 +0100

    Added completed styles for tasks

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,18 +3,29 @@ import React from 'react';
 
 const Todo = ({ text, todo, todos, setTodos }) => {
     const deleteHandler = () => {
-        console.log(todo.id)
         setTodos(todos.filter((element) => element.id !== todo.id));
     };
 
+    const completeHandler = () => {
+        setTodos(todos.map((element) => {
+            if (element.id === todo.id){
+                return{
+                    ...element, completed: !element.completed
+                };
+            }
+            return element;
+        })
+        );
+    }
+
     return(
-        <div className="task-box">
+        <div className={`task-box ${todo.completed ? "completed" : ""}`}>
             <li className="task-item">
                 {text}
             </li>
             <button className="info-button"><i className="fas fa-info-circle"></i></button>
             <button className="priority-button"><i className="fas fa-layer-group"></i></button>
-            <button className="complete-button"><i className="fas fa-check-circle"></i></button>
+            <button onClick={completeHandler} className="complete-button"><i className="fas fa-check-circle"></i></button>
             <button onClick={deleteHandler} className="delete-button"><i className="fas fa-minus-circle"></i></button>
         </div>
     )
