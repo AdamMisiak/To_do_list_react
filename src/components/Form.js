309a5e660e4741482b062f31ef612d0a9d916379
AdamMisiak <adammisiak3@gmail.com>
2020-12-16 15:50:13 +0100

    Added html for project input and filters

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,12 +13,42 @@ const Form = ({ setInputText, inputText, setTodos, todos }) => {
         setInputText("")
     }
     return (
-        <form className="task-form">
-            <input value={inputText} onChange={inputTextHandler} type="text" className="task-input" placeholder="Add task..."></input>
-            <button onClick={submitTodoHandler} className="task-button" type="submit">
-                <span title="Add new task"><i className="fas fa-plus-circle"></i></span>
-            </button>
-        </form>
+        <div>
+            <form className="task-form">
+                <input value={inputText} onChange={inputTextHandler} type="text" className="task-input" placeholder="Add task..."></input>
+                <button onClick={submitTodoHandler} className="task-button" type="submit">
+                    <span title="Add new task"><i className="fas fa-plus-circle"></i></span>
+                </button>
+            </form>
+
+            <form class="project-form">
+                <input type="text" class="project-input" placeholder="Add project..."></input>
+                <button class="project-button" type="submit">
+                    <i class="fas fa-plus-circle"></i>
+                </button>
+            </form>
+
+            <form class="filters">
+            <h3>Priority filter:</h3>
+
+            <div class="select priority">
+                <select name="filter-priority" class="filter-priority"  placeholder="Add project...">
+                    <option value="all">All</option>
+                    <option value="low">Low</option>
+                    <option value="medium">Medium</option>
+                    <option value="high">High</option>
+                </select>
+            </div>
+
+            <h3>Project filter:</h3>
+            <div class="select project">
+                <select name="filter-project" class="filter-project">
+                    <option value="all">All</option>
+                    <option value="none">None</option>
+                </select>
+            </div>
+            </form>
+        </div>
     );
 };
 
