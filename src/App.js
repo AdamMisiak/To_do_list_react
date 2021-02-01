3d4ac87ca1956c1c58ee203ad90b6a44a9ef74fb
AdamMisiak <adammisiak3@gmail.com>
2021-02-01 15:50:05 +0100

    Migrated styles to separated filed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import './styles/App.css';
 
 import Form from './components/Form'
 import TasksContainer from './components/TasksContainer'
